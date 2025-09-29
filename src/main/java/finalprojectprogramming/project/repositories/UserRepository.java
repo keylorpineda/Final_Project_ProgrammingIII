cdc19034f736d2f61057af7293557fb918f3f2c2
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2025-09-28 23:16:07 -0600

    feat(security): implement AppUserDetails and AppUserDetailsService for user authentication

--- src/main/java/finalprojectprogramming/project/repositories/UserRepository.java
+++ src/main/java/finalprojectprogramming/project/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package finalprojectprogramming.project.repositories;
 
 import finalprojectprogramming.project.models.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByAzureId(String azureId);
 }
